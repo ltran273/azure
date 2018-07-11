59d5997241cbda67d907c4201ea1f0b0f229b9c6
Linh <hlinh273@gmail.com>
2018-07-11 10:43:57 +0700

    Initial

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="container-fluid text-center footer-custom">
+            </footer>
+        );
+    }
+}
+
+export default Footer;
